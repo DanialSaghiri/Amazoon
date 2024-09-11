39e2e68db34013eae181441515eb6e1e48b7a6bf
Danial <danial.saghiri83@gmail.com>
2024-09-11 12:14:20 +0330

    complete nav and menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useRef, useState } from "react";
 import axios from "axios";
 import Select from "react-select";
 import { Navigation } from "swiper/modules";
@@ -11,7 +11,14 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import image from "./assets/images/logoAmazoon.avif";
 import image2 from "./assets/images/logoFlag.avif";
-import image3 from "./assets/images/image (5).avif"
+import image3 from "./assets/images/shop.avif";
+import image4 from "./assets/images/logoMenu.avif";
+import image5 from "./assets/images/logoUser.avif";
+import image6 from "./assets/images/slider1.avif";
+import image7 from "./assets/images/slider2.avif";
+import image8 from "./assets/images/slider3.avif";
+import image9 from "./assets/images/slider4.avif";
+
 // const getData = async () => {
 //   const respons = await axios("http://localhost:5000/data");
 //   console.log(respons);
@@ -19,6 +26,13 @@ import image3 from "./assets/images/image (5).avif"
 // getData();
 
 function App() {
+  const [activeMenu, setActiveMenu] = useState("notActive");
+  const [page, setPage] = useState(false);
+  const menuHandler = () => {
+    activeMenu === "active"
+      ? setActiveMenu("notActive")
+      : setActiveMenu("active");
+  };
   return (
     <>
       <header>
@@ -124,25 +138,115 @@ function App() {
             <h3>Cart</h3>
           </div>
         </section>
-        <section></section>
+        <section className="downNav">
+          <div className="menu">
+            <div className="menuBurger" onClick={menuHandler}>
+              <img src={image4} alt="" />
+              <span>All</span>
+            </div>
+            <span>Today's Deals </span>
+            <span>Customer</span>
+            <span>Service</span>
+            <span>Registry</span>
+            <span>Gift Cards Sell</span>
+          </div>
+          <div className={activeMenu} onClick={menuHandler}>
+            <div className="user">
+              <img src={image5} alt="" />
+              <h2>Hello, sign in</h2>
+            </div>
+
+            {!page ? (
+              <>
+                <div className="devices">
+                  <h2>Digital Content & Devices</h2>
+                  <span onClick={() => setPage(!page)}>
+                    Amazon Music
+                    <i className="bx bx-chevron-right"></i>
+                  </span>
+                  <span>
+                    Kindle E -readers & Books
+                    <i className="bx bx-chevron-right"></i>
+                  </span>
+                  <span>
+                    Amazon Appstore
+                    <i className="bx bx-chevron-right"></i>
+                  </span>
+                </div>
+                <div className="department">
+                  <h2>Shop by Department</h2>
+                  <span>
+                    Electronics
+                    <i className="bx bx-chevron-right"></i>
+                  </span>
+                  <span>
+                    Computer
+                    <i className="bx bx-chevron-right"></i>
+                  </span>
+                  <span>
+                    Smart Home
+                    <i className="bx bx-chevron-right"></i>
+                  </span>
+                  <span>
+                    Arts & Crafts
+                    <i className="bx bx-chevron-right"></i>
+                  </span>
+                  <span>
+                    See all
+                    <i className="bx bx-chevron-right"></i>
+                  </span>
+                </div>
+              </>
+            ) : (
+              <div>
+                <span onClick={(c) => setPage(!c)}>
+                  Main Menu
+                  <i className="bx bx-chevron-right"></i>
+                </span>
+                <div className="music">
+                  <h2>Stream Music</h2>
+                  <span>Amazon Music Unlimited</span>
+                  <span> Free Streaming Music</span>
+                  <span>Podcasts</span>
+                  <span> Open Web Player</span>
+                  <span>Download the app</span>
+                </div>
+              </div>
+            )}
+          </div>
+        </section>
       </header>
-      {/* <Swiper
-        // install Swiper modules
-        modules={[Navigation]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-      >
-        <SwiperSlide>
-          <img src={image} alt="" />
-        </SwiperSlide><SwiperSlide>
-          <img src={image} alt="" />
-        </SwiperSlide><SwiperSlide>
-          <img src={image} alt="" />
-        </SwiperSlide><SwiperSlide>
-          <img src={image} alt="" />
-        </SwiperSlide>
-      </Swiper> */}
+      <main className="container">
+        
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={50}
+          slidesPerView={1}
+          className="slider"
+          navigation
+        >
+          <SwiperSlide>
+            <img src={image6} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image7} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image8} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={image9} alt="" />
+          </SwiperSlide>
+        </Swiper>
+        <section className="products">
+          <div className="product"></div>
+          <div className="product"></div>
+          <div className="product"></div>
+          <div className="product"></div>
+          <div className="product"></div>
+          <div className="product"></div>
+        </section>
+      </main>
     </>
   );
 }
